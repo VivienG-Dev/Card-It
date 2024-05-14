0ce0a0549106991271729ab3e065eb1113260408
VivienG-Dev <77700655+VivienG-Dev@users.noreply.github.com>
2024-05-14 02:28:09 +0200

    Update nuxt.config.ts with new meta tags and modules for vercel

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,31 +1,58 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", '@pinia/nuxt'],
+  target: "server",
+  modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt"],
   app: {
     // baseURL: '/star-citizen-tool/',
     head: {
-      title: 'Card-It',
+      title: "Card-It",
       meta: [
-        { charset: 'utf-8' },
-        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        { hid: 'description', name: 'description', content: 'In Star Citizen Tool you can find all the information about the ships in Star Citizen.' },
-        { name: 'keywords', content: 'Star Citizen, Ships, Information' },
-        { property: 'og:title', content: 'Card-It' },
-        { property: 'og:description', content: 'In Star Citizen Tool you can find all the information about the ships in Star Citizen.' },
-        { property: 'og:image', content: '/star-citizen-banner.webp' },
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        {
+          hid: "description",
+          name: "description",
+          content:
+            "In Star Citizen Tool you can find all the information about the ships in Star Citizen.",
+        },
+        { name: "keywords", content: "Star Citizen, Ships, Information" },
+        { property: "og:title", content: "Card-It" },
+        {
+          property: "og:description",
+          content:
+            "In Star Citizen Tool you can find all the information about the ships in Star Citizen.",
+        },
+        { property: "og:image", content: "/star-citizen-banner.webp" },
         // { property: 'og:url', content: 'URL_TO_YOUR_PAGE' },
-        { name: 'twitter:card', content: 'All the information about the ships in Star Citizen.' },
-        { name: 'twitter:site', content: '@vivieng_webdev' },
-        { name: 'twitter:title', content: 'Card-It' },
-        { name: 'twitter:description', content: 'In Star Citizen Tool you can find all the information about the ships in Star Citizen.' },
-        { name: 'twitter:image', content: 'https://www.yourwebsite.com/star-citizen-banner.webp' },
+        {
+          name: "twitter:card",
+          content: "All the information about the ships in Star Citizen.",
+        },
+        { name: "twitter:site", content: "@vivieng_webdev" },
+        { name: "twitter:title", content: "Card-It" },
+        {
+          name: "twitter:description",
+          content:
+            "In Star Citizen Tool you can find all the information about the ships in Star Citizen.",
+        },
+        {
+          name: "twitter:image",
+          content: "https://www.yourwebsite.com/star-citizen-banner.webp",
+        },
       ],
       link: [
-        { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
-        { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },
-        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;900&display=swap' },
+        { rel: "preconnect", href: "https://fonts.googleapis.com" },
+        {
+          rel: "preconnect",
+          href: "https://fonts.gstatic.com",
+          crossorigin: "",
+        },
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;900&display=swap",
+        },
       ],
     },
   },
-})
+});
