d536e3371f0e84809b1b30626a2ce9021f18b7ec
VivienG-Dev <77700655+VivienG-Dev@users.noreply.github.com>
2024-05-17 21:09:45 +0200

    refactor: Updated the localhost link to the VITE_API_URL

--- frontend/components/Navbar.vue
+++ frontend/components/Navbar.vue
@@ -24,7 +24,14 @@ const {
   errorResponse: errorResponseSignIn,
 } = useSignIn();
 
-const { usernameError, emailError, passwordError, validateUsername, validateEmail, validatePassword } = useValidation();
+const {
+  usernameError,
+  emailError,
+  passwordError,
+  validateUsername,
+  validateEmail,
+  validatePassword,
+} = useValidation();
 
 watch(
   () => route.query.modal,
@@ -88,33 +95,45 @@ function useRegister() {
 
   const register = async () => {
     try {
-      const response = await fetch("http://localhost:3001/auth/register", {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          username: username.value.toLowerCase(),
-          email: email.value,
-          password: password.value,
-        }),
-      });
+      const response = await fetch(
+        `${import.meta.env.VITE_API_URL}/auth/register`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            username: username.value.toLowerCase(),
+            email: email.value,
+            password: password.value,
+          }),
+        }
+      );
 
       const responseData = await response.json();
 
       if (response.ok) {
         errorResponse.value = "";
-        successfulResponse.value = responseData.message || "Registration successful!";
+        successfulResponse.value =
+          responseData.message || "Registration successful!";
       } else {
         successfulResponse.value = "";
-        errorResponse.value = responseData.message || "An error occurred. Please try again.";
+        errorResponse.value =
+          responseData.message || "An error occurred. Please try again.";
       }
     } catch (error) {
       errorResponse.value = "Registration failed. Please try again.";
     }
   };
 
-  return { email, password, username, register, successfulResponse, errorResponse };
+  return {
+    email,
+    password,
+    username,
+    register,
+    successfulResponse,
+    errorResponse,
+  };
 }
 
 function useSignIn() {
@@ -126,17 +145,20 @@ function useSignIn() {
 
   const signIn = async () => {
     try {
-      const response = await fetch("http://localhost:3001/auth/sign-in", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          email: email.value,
-          password: password.value,
-        }),
-        credentials: "include",
-      });
+      const response = await fetch(
+        `${import.meta.env.VITE_API_URL}/auth/sign-in`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email: email.value,
+            password: password.value,
+          }),
+          credentials: "include",
+        }
+      );
 
       const responseData = await response.json();
 
@@ -147,7 +169,8 @@ function useSignIn() {
         localStorage.setItem("username", responseData.user.username);
         router.push(`/users/${responseData.user.username}`);
       } else {
-        errorResponse.value = responseData.message || "An error occurred. Please try again.";
+        errorResponse.value =
+          responseData.message || "An error occurred. Please try again.";
       }
     } catch (error) {
       errorResponse.value = "Login failed. Please try again.";
@@ -180,32 +203,57 @@ const links = computed(() => [
 
 <template>
   <div>
-    <nav class="flex items-center justify-between bg-white shadow h-10 rounded-lg py-2 px-4">
+    <nav
+      class="flex items-center justify-between bg-white shadow h-10 rounded-lg py-2 px-4"
+    >
       <nuxt-link to="/">Card-It</nuxt-link>
       <ClientOnly>
         <div class="relative">
-          <div v-if="authStore.isAuthenticated" class="flex space-x-4 space-y-1 font-light">
-            <button @click="toggleMenu" class="focus:outline-none flex sm:hidden">
-              <Icons :isType="isMenuOpen ? 'closeMenu' : 'menu'" svgClass="w-7 h-7" />
+          <div
+            v-if="authStore.isAuthenticated"
+            class="flex space-x-4 space-y-1 font-light"
+          >
+            <button
+              @click="toggleMenu"
+              class="focus:outline-none flex sm:hidden"
+            >
+              <Icons
+                :isType="isMenuOpen ? 'closeMenu' : 'menu'"
+                svgClass="w-7 h-7"
+              />
             </button>
             <div class="hidden sm:flex space-x-4">
               <nuxt-link :to="`/users/${username}`">Dashboard</nuxt-link>
             </div>
-            <button class="hidden sm:flex" @click="authStore.logout">Logout</button>
+            <button class="hidden sm:flex" @click="authStore.logout">
+              Logout
+            </button>
           </div>
           <div v-else class="space-x-4 font-light flex">
             <button @click="openModal(true)">Register</button>
             <button @click="openModal(false)">Login</button>
           </div>
-          <div v-if="isMenuOpen" class="absolute -right-3 mt-3 w-48 rounded-md shadow-lg bg-white z-50">
+          <div
+            v-if="isMenuOpen"
+            class="absolute -right-3 mt-3 w-48 rounded-md shadow-lg bg-white z-50"
+          >
             <div v-if="authStore.isAuthenticated" class="py-1">
-              <nuxtLink v-for="link in links" :key="link.name" :to="link.path"
+              <nuxtLink
+                v-for="link in links"
+                :key="link.name"
+                :to="link.path"
                 class="flex flex-col w-full text-left px-4 py-2 text-gray-700 hover:bg-gray-100"
-                @click.native="isMenuOpen = false">
+                @click.native="isMenuOpen = false"
+              >
                 {{ link.name }}
               </nuxtLink>
-              <button @click="authStore.logout" class="block w-full text-left px-4 py-2 text-gray-700 hover:bg-gray-100"
-                @click.native="isMenuOpen = false">Logout</button>
+              <button
+                @click="authStore.logout"
+                class="block w-full text-left px-4 py-2 text-gray-700 hover:bg-gray-100"
+                @click.native="isMenuOpen = false"
+              >
+                Logout
+              </button>
             </div>
           </div>
         </div>
@@ -218,54 +266,109 @@ const links = computed(() => [
 
       <!-- Modal content -->
       <div class="fixed inset-0 flex items-center justify-center p-4">
-        <div class="bg-white rounded-lg shadow-lg py-3 px-14 w-[500px] h-[500px] mx-auto relative">
-          <Icons @click="closeModal" svgClass="w-7 h-7 absolute top-2 right-2" isType="close" />
+        <div
+          class="bg-white rounded-lg shadow-lg py-3 px-14 w-[500px] h-[500px] mx-auto relative"
+        >
+          <Icons
+            @click="closeModal"
+            svgClass="w-7 h-7 absolute top-2 right-2"
+            isType="close"
+          />
           <!-- Register Form -->
           <div v-if="showRegisterForm" class="flex flex-col">
-            <h2 class="text-lg text-center font-semibold text-gray-800">Create a new account</h2>
-            <div class="flex justify-center items-center w-full h-10 rounded-lg text-sm" :class="{
-              'bg-red-100 text-red-500': errorResponseRegister,
-              'bg-green-100 text-green-500': successfulResponseRegister,
-            }">
+            <h2 class="text-lg text-center font-semibold text-gray-800">
+              Create a new account
+            </h2>
+            <div
+              class="flex justify-center items-center w-full h-10 rounded-lg text-sm"
+              :class="{
+                'bg-red-100 text-red-500': errorResponseRegister,
+                'bg-green-100 text-green-500': successfulResponseRegister,
+              }"
+            >
               {{ errorResponseRegister || successfulResponseRegister }}
             </div>
             <form @submit.prevent="register" class="space-y-4">
               <div>
-                <label for="Username" class="font-normal text-sm mb-10">Username:</label>
-                <input type="text" placeholder="Username" name="Username" ref="usernameRef" v-model="usernameRegister"
+                <label for="Username" class="font-normal text-sm mb-10"
+                  >Username:</label
+                >
+                <input
+                  type="text"
+                  placeholder="Username"
+                  name="Username"
+                  ref="usernameRef"
+                  v-model="usernameRegister"
                   @input="validateUsername(usernameRegister)"
                   class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-                  :class="{ 'border-red-500': usernameError || (errorResponseRegister && usernameRegister === '') }" />
-                <div class="text-red-500 text-xs text-center h-2">{{ usernameError }}</div>
+                  :class="{
+                    'border-red-500':
+                      usernameError ||
+                      (errorResponseRegister && usernameRegister === ''),
+                  }"
+                />
+                <div class="text-red-500 text-xs text-center h-2">
+                  {{ usernameError }}
+                </div>
               </div>
               <div>
                 <label for="Email" class="font-normal text-sm">Email:</label>
-                <input type="email" placeholder="Email" name="Email" v-model="emailRegister"
+                <input
+                  type="email"
+                  placeholder="Email"
+                  name="Email"
+                  v-model="emailRegister"
                   @input="validateEmail(emailRegister)"
                   class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-                  :class="{ 'border-red-500': emailError || (errorResponseRegister && emailRegister === '') }" />
-                <div class="text-red-500 text-xs text-center h-2">{{ emailError }}</div>
+                  :class="{
+                    'border-red-500':
+                      emailError ||
+                      (errorResponseRegister && emailRegister === ''),
+                  }"
+                />
+                <div class="text-red-500 text-xs text-center h-2">
+                  {{ emailError }}
+                </div>
               </div>
               <div>
-                <label for="Password" class="font-normal text-sm">Password:</label>
+                <label for="Password" class="font-normal text-sm"
+                  >Password:</label
+                >
                 <div class="relative flex items-center">
-                  <input id="Password" :type="isPasswordVisible ? 'text' : 'password'" placeholder="Password"
-                    v-model="passwordRegister" @input="validatePassword(passwordRegister)"
+                  <input
+                    id="Password"
+                    :type="isPasswordVisible ? 'text' : 'password'"
+                    placeholder="Password"
+                    v-model="passwordRegister"
+                    @input="validatePassword(passwordRegister)"
                     class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-                    :class="{ 'border-red-500': passwordErrorRegister || (errorResponseRegister && passwordRegister === '') }" />
-                  <Icons @click="togglePasswordVisibility()"
+                    :class="{
+                      'border-red-500':
+                        passwordErrorRegister ||
+                        (errorResponseRegister && passwordRegister === ''),
+                    }"
+                  />
+                  <Icons
+                    @click="togglePasswordVisibility()"
                     svgClass="w-6 h-6 absolute right-2 top-1/2 transform -translate-y-1/2"
-                    :isType="isPasswordVisible ? 'eye' : 'eyeSlash'" />
+                    :isType="isPasswordVisible ? 'eye' : 'eyeSlash'"
+                  />
                 </div>
 
-                <div class="text-red-500 text-xs text-center h-4">{{ passwordError }}</div>
+                <div class="text-red-500 text-xs text-center h-4">
+                  {{ passwordError }}
+                </div>
               </div>
               <Button name="Register" />
               <div class="flex justify-center">
                 <div class="w-2/3 h-px bg-black rounded-lg"></div>
               </div>
               <p class="text-sm text-center font-extralight">
-                Already have an account? <NuxtLink to="/?modal=login" class="underline underline-offset-2">Sign-In
+                Already have an account?
+                <NuxtLink
+                  to="/?modal=login"
+                  class="underline underline-offset-2"
+                  >Sign-In
                 </NuxtLink>
               </p>
             </form>
@@ -273,42 +376,81 @@ const links = computed(() => [
 
           <!-- Login Form -->
           <div v-else class="space-y-4">
-            <h2 class="text-lg text-center font-semibold text-gray-800">Sign-In</h2>
-            <div class="flex justify-center items-center w-full h-10 rounded-lg text-sm"
-              :class="{ 'bg-red-100 text-red-500': errorResponseSignIn, 'bg-green-100 text-green-500': successfulResponseSignIn }">
+            <h2 class="text-lg text-center font-semibold text-gray-800">
+              Sign-In
+            </h2>
+            <div
+              class="flex justify-center items-center w-full h-10 rounded-lg text-sm"
+              :class="{
+                'bg-red-100 text-red-500': errorResponseSignIn,
+                'bg-green-100 text-green-500': successfulResponseSignIn,
+              }"
+            >
               {{ errorResponseSignIn || successfulResponseSignIn }}
             </div>
             <form @submit.prevent="signIn" class="space-y-4">
               <div>
                 <label for="Email">Email:</label>
-                <input type="email" placeholder="Email" ref="emailRef" v-model="emailSignIn"
+                <input
+                  type="email"
+                  placeholder="Email"
+                  ref="emailRef"
+                  v-model="emailSignIn"
                   @input="validateEmail(emailSignIn)"
                   class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-                  :class="{ 'border-red-500': emailError || (errorResponseSignIn && emailSignIn === '') }" />
-                <div class="text-red-500 text-xs text-center h-4">{{ emailError }}</div>
+                  :class="{
+                    'border-red-500':
+                      emailError || (errorResponseSignIn && emailSignIn === ''),
+                  }"
+                />
+                <div class="text-red-500 text-xs text-center h-4">
+                  {{ emailError }}
+                </div>
               </div>
               <div>
                 <label for="Password">Password:</label>
                 <div class="relative flex items-center">
-                  <input id="Password" :type="isPasswordVisible ? 'text' : 'password'" placeholder="Password"
-                    v-model="passwordSignIn" @input="validatePassword(passwordSignIn)"
+                  <input
+                    id="Password"
+                    :type="isPasswordVisible ? 'text' : 'password'"
+                    placeholder="Password"
+                    v-model="passwordSignIn"
+                    @input="validatePassword(passwordSignIn)"
                     class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-                    :class="{ 'border-red-500': passwordError || (errorResponseSignIn && passwordSignIn === '') }" />
-                  <Icons @click="togglePasswordVisibility()"
+                    :class="{
+                      'border-red-500':
+                        passwordError ||
+                        (errorResponseSignIn && passwordSignIn === ''),
+                    }"
+                  />
+                  <Icons
+                    @click="togglePasswordVisibility()"
                     svgClass="w-6 h-6 absolute right-2 top-1/2 transform -translate-y-1/2"
-                    :isType="isPasswordVisible ? 'eye' : 'eyeSlash'" />
+                    :isType="isPasswordVisible ? 'eye' : 'eyeSlash'"
+                  />
                 </div>
-                <div class="text-red-500 text-xs text-center h-8">{{ passwordError }}</div>
+                <div class="text-red-500 text-xs text-center h-8">
+                  {{ passwordError }}
+                </div>
               </div>
               <Button name="Sign-In" />
               <div class="flex justify-center">
                 <div class="w-2/3 h-px bg-black rounded-lg"></div>
               </div>
-              <p class="text-sm text-center font-extralight">No account? <NuxtLink to="/?modal=register"
-                  class="underline underline-offset-2">Register</NuxtLink>
+              <p class="text-sm text-center font-extralight">
+                No account?
+                <NuxtLink
+                  to="/?modal=register"
+                  class="underline underline-offset-2"
+                  >Register</NuxtLink
+                >
               </p>
               <p class="text-sm text-center font-extralight">
-                <NuxtLink to="/forgot-password" class="underline underline-offset-2">Forgot password</NuxtLink>
+                <NuxtLink
+                  to="/forgot-password"
+                  class="underline underline-offset-2"
+                  >Forgot password</NuxtLink
+                >
               </p>
             </form>
           </div>
