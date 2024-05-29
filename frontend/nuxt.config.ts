ac55b5fdf00f38cae778dc97a905e6518a87fb8f
VivienG-Dev <77700655+VivienG-Dev@users.noreply.github.com>
2024-05-29 22:21:44 +0200

    refactor: Disable devtools in nuxt.config.ts

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   ssr: true,
   modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt"],
   app: {
