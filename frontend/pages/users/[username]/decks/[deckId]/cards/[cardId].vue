b3a5d7dc9ac35d7530239eb6ae8ced3584068a6f
VivienG-Dev <77700655+VivienG-Dev@users.noreply.github.com>
2024-06-12 17:00:40 +0200

    refactor: Use custom hook useApiFetch for API requests

--- frontend/pages/users/[username]/decks/[deckId]/cards/[cardId].vue
+++ frontend/pages/users/[username]/decks/[deckId]/cards/[cardId].vue
@@ -1,164 +1,76 @@
 <script setup>
-// definePageMeta({
-//   middleware: "auth",
-// });
-
-import { ref, onMounted } from "vue";
+import { ref } from "vue";
 import { useRoute } from "vue-router";
 
 const route = useRoute();
+const router = useRouter();
 
-const username = ref(route.params.username);
-const deckId = ref(route.params.deckId);
-const cardId = ref(route.params.cardId);
+const username = route.params.username;
+const deckId = route.params.deckId;
+const cardId = route.params.cardId;
 
-const { titleError, descriptionError, validateTitle, validateDescription } =
-  useValidation();
+const { titleError, descriptionError, validateTitle, validateDescription } = useValidation();
 
 function goBack() {
-  navigateTo(`/users/${username.value}/decks/${deckId.value}`);
+  navigateTo(`/users/${username}/decks/${deckId}`);
 }
 
 const title = ref("");
 const description = ref("");
 const updateCardSuccess = ref(null);
 const updateCardError = ref(null);
-const updateLoadingCard = ref(false);
-function updateCard() {
+async function updateCard() {
+  updateCardError.value = null;
+  updateCardSuccess.value = null;
+
   if (titleError.value || descriptionError.value) {
     updateCardError.value = "Please correct the errors before submitting.";
     return;
   }
 
-  updateCardSuccess.value = null;
-  updateCardError.value = null;
-  updateLoadingCard.value = true;
+  const updateCardApiUrl = `${import.meta.env.VITE_API_URL}/users/${username}/decks/${deckId}/cards/${cardId}`;
+  const updateCardState = await $fetchApi(updateCardApiUrl, {
+    method: "PATCH",
+    body: JSON.stringify({
+      title: title.value,
+      description: description.value,
+    }),
+  });
 
-  const body = {
-    title: title.value || cardData.value.title,
-    description: description.value || cardData.value.description,
-  };
-
-  if (
-    body.title === cardData.value.title &&
-    body.description === cardData.value.description
-  ) {
-    updateCardError.value = "No changes detected";
-    updateLoadingCard.value = false;
-    return;
+  if (updateCardState.data) {
+    updateCardSuccess.value = "Card updated successfully";
+  } else if (updateCardState.error) {
+    updateCardError.value = updateCardState.error;
   }
-
-  fetch(
-    `${import.meta.env.VITE_API_URL}/users/${username.value}/decks/${
-      deckId.value
-    }/cards/${cardId.value}`,
-    {
-      method: "PATCH",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(body),
-      credentials: "include",
-    }
-  )
-    .then((response) => {
-      if (!response.ok) {
-        return response.json().then((body) => {
-          const errorMessage = body.error?.message || "Unknown error occurred";
-          throw new Error(errorMessage);
-        });
-      }
-      return response.json();
-    })
-    .then((data) => {
-      updateCardSuccess.value = data.message;
-      cardData.value = data.data;
-    })
-    .catch((err) => {
-      updateCardError.value = err.message;
-    })
-    .finally(() => {
-      updateLoadingCard.value = false;
-    });
 }
 
 const showModal = ref(false);
 function hideModal() {
   showModal.value = false;
 }
-function deleteCard() {
-  const error = ref(null);
-  const loading = ref(true);
 
-  fetch(
-    `${import.meta.env.VITE_API_URL}/users/${username.value}/decks/${
-      deckId.value
-    }/cards/${cardId.value}`,
-    {
-      method: "DELETE",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      credentials: "include",
-    }
-  )
-    .then((response) => {
-      if (!response.ok) {
-        return response.json().then((body) => {
-          const errorMessage =
-            body.error?.message ||
-            response.statusText ||
-            "Unknown error occurred";
-          throw new Error(errorMessage);
-        });
-      }
+const cardError = ref(null);
+async function deleteCard() {
+  const deleteCardApiUrl = `${import.meta.env.VITE_API_URL}/users/${username}/decks/${deckId}/cards/${cardId}`;
+  const deleteCardState = await $fetchApi(deleteCardApiUrl, {
+    method: "DELETE",
+  });
 
-      showModal.value = false;
-      navigateTo(`/users/${username.value}/decks/${deckId.value}`);
-    })
-    .catch((err) => {
-      error.value = err.message;
-    })
-    .finally(() => {
-      loading.value = false;
-    });
+  if (deleteCardState.data) {
+    router.push(`/users/${username}/decks/${deckId}`);
+  } else {
+    cardError.value = deleteCardState.error || "An error occurred while deleting the card";
+  }
 }
 
-const cardData = ref({});
-const cardError = ref(null);
-const cardLoading = ref(false);
-onMounted(() => {
-  // Fetch the card details
-  cardLoading.value = true;
-  fetch(
-    `${import.meta.env.VITE_API_URL}/users/${username.value}/decks/${
-      deckId.value
-    }/cards/${cardId.value}`,
-    {
-      method: "GET",
-      credentials: "include",
-    }
-  )
-    .then((response) => {
-      if (!response.ok) {
-        return response.json().then((body) => {
-          const errorMessage = body.error?.message || "Unknown error occurred";
-          throw new Error(errorMessage);
-        });
-      }
-      return response.json();
-    })
-    .then((data) => {
-      cardData.value = data.data;
-      title.value = data.data.title;
-      description.value = data.data.description;
-    })
-    .catch((err) => {
-      cardError.value = err.message;
-    })
-    .finally(() => {
-      cardLoading.value = false;
-    });
+const cardApiUrl = `${import.meta.env.VITE_API_URL}/users/${username}/decks/${deckId}/cards/${cardId}`;
+const cardState = useApiFetch(cardApiUrl);
+
+watch(cardState, (newState) => {
+  if (newState.data) {
+    title.value = newState.data.title;
+    description.value = newState.data.description;
+  }
 });
 </script>
 
@@ -166,20 +78,13 @@ onMounted(() => {
   <div class="flex flex-col space-y-16">
     <div class="flex justify-between">
       <div class="w-28">
-        <Icons
-          @click="goBack()"
-          svgClass="w-8 h-8 cursor-pointer"
-          isType="arrowLeft"
-        />
+        <Icons @click="goBack()" svgClass="w-8 h-8 cursor-pointer" isType="arrowLeft" />
       </div>
       <h2 class="text-center text-xl font-bold w-full">Dashboard</h2>
       <div class="w-28" />
     </div>
     <div class="flex flex-col items-center w-full space-y-4">
-      <form
-        @submit.prevent="updateCard()"
-        class="space-y-4 w-full sm:w-[450px]"
-      >
+      <form @submit.prevent="updateCard()" class="space-y-4 w-full sm:w-[450px]">
         <div class="flex flex-col space-y-2">
           <div
             class="flex justify-center items-center w-full h-10 rounded-lg text-sm"
@@ -197,12 +102,10 @@ onMounted(() => {
             id="title"
             name="title"
             v-model="title"
-            :placeholder="cardData.title"
             @input="validateTitle(title)"
             class="border border-gray-300 rounded-lg p-2"
             :class="{
-              'border-red-500':
-                updateCardError === 'Title cannot be empty' || titleError,
+              'border-red-500': updateCardError === 'Title cannot be empty' || titleError,
             }"
           />
           <div class="text-red-500 text-xs text-center h-2">
@@ -210,9 +113,7 @@ onMounted(() => {
           </div>
         </div>
         <div class="flex flex-col space-y-2">
-          <label for="description" class="text-sm font-light"
-            >Card Description</label
-          >
+          <label for="description" class="text-sm font-light">Card Description</label>
           <textarea
             type="text"
             id="description"
@@ -221,9 +122,7 @@ onMounted(() => {
             @input="validateDescription(description)"
             class="border border-gray-300 rounded-lg p-2"
             :class="{
-              'border-red-500':
-                updateCardError === 'Description cannot be empty' ||
-                descriptionError,
+              'border-red-500': updateCardError === 'Description cannot be empty' || descriptionError,
             }"
           />
           <div class="text-red-500 text-xs text-center h-2">
@@ -235,17 +134,8 @@ onMounted(() => {
         </div>
       </form>
       <div class="text-center">
-        <button
-          @click="showModal = true"
-          class="text-red-600 text-base font-light"
-        >
-          Delete the card
-        </button>
-        <Modal
-          :isVisible="showModal"
-          @confirm="deleteCard()"
-          @cancel="hideModal()"
-        >
+        <button @click="showModal = true" class="text-red-600 text-base font-light">Delete the card</button>
+        <Modal :isVisible="showModal" @confirm="deleteCard()" @cancel="hideModal()">
           <template #content>
             <p>Are you sure you want to delete this card?</p>
           </template>
