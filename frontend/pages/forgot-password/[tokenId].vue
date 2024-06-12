297179eec5798939511241778c0a9cd093104036
VivienG-Dev <77700655+VivienG-Dev@users.noreply.github.com>
2024-06-12 18:06:05 +0200

    refactor: Use custom hook useApiFetch for API requests

--- frontend/pages/forgot-password/[tokenId].vue
+++ frontend/pages/forgot-password/[tokenId].vue
@@ -7,51 +7,27 @@ const route = useRoute();
 
 const { passwordError, validatePassword } = useValidation();
 
+const tokenId = route.params.tokenId;
+
 const newPassword = ref("");
 const confirmPassword = ref("");
-const tokenId = ref(route.params.tokenId);
 const sendingError = ref("");
 const sendingSuccess = ref("");
-const loading = ref(false);
-function updateUserPassword() {
+async function updateUserPassword() {
   sendingError.value = "";
   sendingSuccess.value = "";
-  loading.value = true;
-  fetch(
-    `${import.meta.env.VITE_API_URL}/auth/forgot-password/${tokenId.value}`,
-    {
-      method: "PATCH",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        newPassword: newPassword.value,
-        confirmPassword: confirmPassword.value,
-      }),
-      credentials: "include",
-    }
-  )
-    .then((response) => {
-      if (!response.ok) {
-        return response.json().then((body) => {
-          const errorMessage = body.error?.message || "Unknown error occurred";
-          throw new Error(errorMessage);
-        });
-      }
-      return response.json();
-    })
-    .then((_) => {
-      sendingSuccess.value =
-        "Password updated successfully, you may now login.";
-      // navigateTo("/");
-    })
-    .catch((err) => {
-      sendingError.value =
-        err.message || "Failed to update password. Please try again.";
-    })
-    .finally(() => {
-      loading.value = false;
-    });
+
+  const updateUserPasswordApiUrl = `${import.meta.env.VITE_API_URL}/auth/forgot-password/${tokenId}`;
+  const updateUserPasswordState = await $fetchApi(updateUserPasswordApiUrl, {
+    method: "PATCH",
+    body: JSON.stringify({ newPassword: newPassword.value, confirmPassword: confirmPassword.value }),
+  });
+
+  if (updateUserPasswordState.data) {
+    sendingSuccess.value = "Password updated successfully, you may now login.";
+  } else {
+    sendingError.value = updateUserPasswordState?.error || "Failed to update password. Please try again.";
+  }
 }
 
 const usePasswordVisibility = () => {
@@ -61,22 +37,14 @@ const usePasswordVisibility = () => {
   };
   return { isVisible, toggleVisibility };
 };
-const {
-  isVisible: isNewPasswordVisible,
-  toggleVisibility: toggleNewPasswordVisibility,
-} = usePasswordVisibility();
-const {
-  isVisible: isConfirmPasswordVisible,
-  toggleVisibility: toggleConfirmPasswordVisibility,
-} = usePasswordVisibility();
+const { isVisible: isNewPasswordVisible, toggleVisibility: toggleNewPasswordVisibility } = usePasswordVisibility();
+const { isVisible: isConfirmPasswordVisible, toggleVisibility: toggleConfirmPasswordVisibility } =
+  usePasswordVisibility();
 </script>
 
 <template>
   <div class="flex flex-col items-center w-full space-y-4">
-    <form
-      @submit.prevent="updateUserPassword(newPassword, confirmPassword)"
-      class="space-y-4 w-full sm:w-[450px]"
-    >
+    <form @submit.prevent="updateUserPassword()" class="space-y-4 w-full sm:w-[450px]">
       <div class="flex flex-col space-y-2">
         <h1 class="text-2xl font-semibold">Change your password</h1>
         <p class="text-sm font-light">Please enter your new password below.</p>
@@ -114,16 +82,14 @@ const {
           {{ passwordError }}
         </div>
 
-        <label for="confirmPassword" class="text-sm font-light"
-          >Confirm password</label
-        >
+        <label for="confirmPassword" class="text-sm font-light">Confirm password</label>
         <div class="relative flex items-center">
           <input
             id="confirmPassword"
             :type="isConfirmPasswordVisible ? 'text' : 'password'"
             placeholder="Confirm password"
             v-model="confirmPassword"
-            @input="confirmPassword"
+            @input="validatePassword(confirmPassword)"
             class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
             :class="{
               'border-red-500': newPassword !== confirmPassword,
