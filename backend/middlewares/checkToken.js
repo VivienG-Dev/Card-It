9d0c99da4189b3c90819a84edd413e22d87a49c8
VivienG-Dev <77700655+VivienG-Dev@users.noreply.github.com>
2024-06-18 11:07:27 +0200

    refactor: Update checkTokenMiddleware to handle token and refreshToken validation

--- backend/middlewares/checkToken.js
+++ backend/middlewares/checkToken.js
@@ -4,6 +4,11 @@ const { AuthenticationError } = require("../helpers/errors/customError");
 
 const checkTokenMiddleware = (req, res, next) => {
   let token = req.cookies.authToken;
+  let refreshToken = req.cookies.refreshToken;
+
+  if (!token && !refreshToken) {
+    throw new AuthenticationError("User not logged in");
+  }
 
   // Check for token in Authorization header if not found in cookies
   if (!token && req.headers.authorization) {
@@ -14,26 +19,61 @@ const checkTokenMiddleware = (req, res, next) => {
     }
   }
 
-  if (!token) {
+  if (token) {
+    jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
+      if (err || !decodedToken.id) {
+        throw new AuthenticationError("Invalid token");
+      } else {
+        req.userId = decodedToken.id;
+        req.username = decodedToken.username;
+        req.email = decodedToken.email;
+        req.emailVerified = decodedToken.emailVerified;
+
+        next();
+      }
+    });
+  } else {
+    verifyRefreshToken(refreshToken, req, res, next);
+  }
+};
+
+function verifyRefreshToken(refreshToken, req, res, next) {
+  if (refreshToken) {
+    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, decodedRefreshToken) => {
+      if (err || !decodedRefreshToken.id) {
+        throw new AuthenticationError("Invalid refresh token");
+      }
+
+      const token = jwt.sign(
+        {
+          id: decodedRefreshToken.id,
+          username: decodedRefreshToken.username,
+          email: decodedRefreshToken.email,
+          emailVerified: decodedRefreshToken.emailVerified,
+        },
+        process.env.JWT_SECRET,
+        {
+          expiresIn: process.env.JWT_EXPIRES_IN,
+        }
+      );
+
+      res.cookie("authToken", token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "None",
+        maxAge: 30 * 60 * 1000,
+      });
+
+      req.userId = decodedRefreshToken.id;
+      req.username = decodedRefreshToken.username;
+      req.email = decodedRefreshToken.email;
+      req.emailVerified = decodedRefreshToken.emailVerified;
+
+      next();
+    });
+  } else {
     throw new AuthenticationError("No token provided");
   }
-
-  jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
-    if (err) {
-      throw new AuthenticationError("Invalid token");
-    }
-
-    if (!decodedToken.id) {
-      throw new AuthenticationError("Invalid token - ID not found");
-    }
-
-    req.userId = decodedToken.id;
-    req.username = decodedToken.username;
-    req.email = decodedToken.email;
-    req.emailVerified = decodedToken.emailVerified;
-
-    next();
-  });
-};
+}
 
 module.exports = checkTokenMiddleware;
