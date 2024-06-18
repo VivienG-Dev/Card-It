832cbca99605ed69c648f160d8bae9ca93b677ba
VivienG-Dev <77700655+VivienG-Dev@users.noreply.github.com>
2024-06-18 15:45:06 +0200

    refactor: Remove unused code and files related to user data and data deletion

--- frontend/middleware/auth.js
+++
@@ -1,8 +0,0 @@
-// export default defineNuxtRouteMiddleware(async (to, from) => {
-//   const authStore = useAuthStore();
-//   if (process.client) {
-//     if (!authStore.isAuthenticated) {
-//       return navigateTo("/404");
-//     }
-//   }
-// });
