d521408466764370081845d5d9499bc6063b83c0
VivienG-Dev <77700655+VivienG-Dev@users.noreply.github.com>
2024-06-21 19:00:20 +0200

    refactor: Added the new button component

--- frontend/pages/users/[username]/index.vue
+++ frontend/pages/users/[username]/index.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, onMounted } from "vue";
+import { ref } from "vue";
 import { useRoute } from "vue-router";
 
 const route = useRoute();
@@ -10,17 +10,12 @@ const username = route.params.username;
 const decksApiUrl = `${import.meta.env.VITE_API_URL}/users/${username}/decks`;
 const deckState = useApiFetch(decksApiUrl);
 
-const {
-  isVisible: isModalAcceptSharedDeckVisible,
-  toggle: toggleModalAcceptSharedDeck,
-} = useModal();
+const { isVisible: isModalAcceptSharedDeckVisible, toggle: toggleModalAcceptSharedDeck } = useModal();
 
 // Accept shared deck
 const token = ref("");
 const acceptSharedDeck = async () => {
-  const acceptShareUrl = `${
-    import.meta.env.VITE_API_URL
-  }/users/${username}/decks/accept-share/${token.value}`;
+  const acceptShareUrl = `${import.meta.env.VITE_API_URL}/users/${username}/decks/accept-share/${token.value}`;
   const acceptShareState = await $fetchApi(acceptShareUrl, {
     method: "PUT",
     body: JSON.stringify({ token: token.value }),
@@ -47,12 +42,7 @@ const acceptSharedDeck = async () => {
   <div class="flex justify-between">
     <p>Deck list:</p>
     <div>
-      <button
-        @click="toggleModalAcceptSharedDeck"
-        class="bg-customPrimary text-white px-4 rounded-lg"
-      >
-        Accept shared link
-      </button>
+      <Button @click="toggleModalAcceptSharedDeck" name="Accept shared link" variant="action" />
       <Modal
         :isVisible="isModalAcceptSharedDeckVisible"
         @confirm="acceptSharedDeck()"
@@ -88,10 +78,7 @@ const acceptSharedDeck = async () => {
     <Icons svgClass="w-8 h-8" isType="locked" />
     <p class="text-2xl font-bold">{{ deckState.error }}</p>
   </div>
-  <div
-    v-else
-    class="bg-white flex flex-wrap gap-4 justify-center sm:justify-normal"
-  >
+  <div v-else class="bg-white flex flex-wrap gap-4 justify-center sm:justify-normal">
     <nuxt-link :to="`/users/${username}/decks`">
       <div
         class="bg-gray-300 w-36 h-48 sm:w-40 sm:h-52 rounded-lg flex justify-center items-center hover:ring-2 ring-blue-500 ring-offset-2 ring-offset-blue-100 cursor-pointer"
