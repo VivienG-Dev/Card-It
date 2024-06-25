5694d1136f6528ffc99e6b82a898fe6e66fad73c
VivienG-Dev <77700655+VivienG-Dev@users.noreply.github.com>
2024-06-25 03:53:48 +0200

    refactor: Create AuthenticatedContent component for improved layout structure

---
+++ frontend/components/AuthenticatedContent.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="flex flex-col sm:flex-row space-x-0 sm:space-x-4 w-full mt-20 sm:mt-32">
+    <Sidebar />
+    <div class="bg-white w-full rounded-lg p-3 space-y-4">
+      <slot />
+    </div>
+  </div>
+</template>
