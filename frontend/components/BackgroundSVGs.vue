590c8d5deeeacfbd883a660e628893e3d4e08808
VivienG-Dev <77700655+VivienG-Dev@users.noreply.github.com>
2024-06-25 03:12:47 +0200

    refactor: Add BackgroundSVGs component for improved code organization and reusability

---
+++ frontend/components/BackgroundSVGs.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <div class="svg-bg svg-bg-1"></div>
+    <div class="svg-bg svg-bg-3"></div>
+    <div class="svg-bg svg-bg-2"></div>
+    <div class="svg-bg svg-bg-4"></div>
+  </div>
+</template>
+
+<style scoped>
+.svg-bg {
+  position: absolute;
+  z-index: -1;
+  filter: blur(70px);
+  transform: translate3d(0, 0, 0); /* Fix for Safari flickering */
+  background-repeat: no-repeat;
+  width: 70%;
+  height: 60%;
+  will-change: filter;
+}
+
+.svg-bg-1 {
+  top: 20%;
+  left: 0%;
+  background-image: url("/Vector-1.svg");
+}
+
+.svg-bg-2 {
+  top: 13%;
+  left: 25%;
+  background-image: url("/Vector-2.svg");
+}
+
+.svg-bg-3 {
+  top: 10%;
+  left: 30%;
+  background-image: url("/Vector-3.svg");
+}
+
+.svg-bg-4 {
+  top: 10%;
+  left: 30%;
+  background-image: url("/Vector-4.svg");
+}
+</style>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -11,10 +11,7 @@ const expandDiv = ref(false);
     </div>
 
     <header class="flex items-center h-screen relative">
-      <div class="svg-bg svg-bg-1"></div>
-      <div class="svg-bg svg-bg-3"></div>
-      <div class="svg-bg svg-bg-2"></div>
-      <div class="svg-bg svg-bg-4"></div>
+      <BackgroundSVGs />
       <div class="flex flex-col sm:flex-row justify-center items-center w-full h-80 px-0 sm:px-10 md:px-20 lg:px-32">
         <div class="w-100 sm:w-1/2 flex flex-col justify-center items-start mb-10">
           <h1 class="text-4xl sm:text-5xl font-bold mb-5">Card-It</h1>
@@ -105,45 +102,6 @@ const expandDiv = ref(false);
 .float-animation {
   animation: floatXY 9s ease-in-out infinite;
   animation-delay: 0.5s;
-}
-
-.svg-bg {
-  position: absolute;
-  z-index: -1;
-  filter: blur(70px);
-  transform: translate3d(0, 0, 0); /* Fix for Safari flickering */
-  background-repeat: no-repeat;
-  width: 70%;
-  height: 60%;
-  will-change: filter;
-}
-
-.svg-bg-1 {
-  top: 20%;
-  left: 0%;
-  background-image: url("/Vector-1.svg");
-  /* animation: one 10s infinite; */
-}
-
-.svg-bg-2 {
-  top: 13%;
-  left: 25%;
-  background-image: url("/Vector-2.svg");
-  /* animation: two 10s infinite delay(0.5s); */
-}
-
-.svg-bg-3 {
-  top: 10%;
-  left: 30%;
-  background-image: url("/Vector-3.svg");
-  /* animation: one 10s infinite; */
-}
-
-.svg-bg-4 {
-  top: 10%;
-  left: 30%;
-  background-image: url("/Vector-4.svg");
-  /* animation: two 10s infinite delay(0.5s); */
 }
 
 @keyframes one {
