bcf3f1e621fc46dc4dab4eda5484d5b4391a523b
VivienG-Dev <77700655+VivienG-Dev@users.noreply.github.com>
2024-06-25 03:15:04 +0200

    refactor: Add BackgroundSVGs component for improved code organization and reusability

--- frontend/layouts/authenticated.vue
+++ frontend/layouts/authenticated.vue
@@ -25,10 +25,7 @@ const links = [
     </div>
 
     <main class="flex relative">
-      <div class="svg-bg svg-bg-1"></div>
-      <div class="svg-bg svg-bg-3"></div>
-      <div class="svg-bg svg-bg-2"></div>
-      <div class="svg-bg svg-bg-4"></div>
+      <BackgroundSVGs />
       <ClientOnly>
         <div
           v-if="authStore.isAuthenticated"
@@ -75,50 +72,3 @@ const links = [
     </div>
   </div>
 </template>
-
-<style scoped>
-.svg-bg {
-  position: absolute;
-  z-index: -1;
-  filter: blur(70px);
-  transform: translate3d(0, 0, 0);
-  /* Fix for Safari flickering */
-  background-repeat: no-repeat;
-  width: 70%;
-  height: 60%;
-  will-change: filter;
-}
-
-.svg-bg-1 {
-  top: 20%;
-  left: 0%;
-  background-image: url("/Vector-1.svg");
-  /* animation: one 10s infinite; */
-}
-
-.svg-bg-2 {
-  top: 13%;
-  left: 25%;
-  background-image: url("/Vector-2.svg");
-  /* animation: two 10s infinite delay(0.5s); */
-}
-
-.svg-bg-3 {
-  top: 10%;
-  left: 30%;
-  background-image: url("/Vector-3.svg");
-  /* animation: one 10s infinite; */
-}
-
-.svg-bg-4 {
-  top: 10%;
-  left: 30%;
-  background-image: url("/Vector-4.svg");
-  /* animation: two 10s infinite delay(0.5s); */
-}
-
-/* .router-link-exact-active {
-  background-color: #90c0f7;
-  color: #fff;
-} */
-</style>
--- frontend/layouts/public.vue
+++ frontend/layouts/public.vue
@@ -7,10 +7,7 @@
     </div>
 
     <main class="flex h-screen relative">
-      <div class="svg-bg svg-bg-1"></div>
-      <div class="svg-bg svg-bg-3"></div>
-      <div class="svg-bg svg-bg-2"></div>
-      <div class="svg-bg svg-bg-4"></div>
+      <BackgroundSVGs />
       <ClientOnly>
         <div class="flex space-x-4 w-full mt-32">
           <div class="bg-white w-full h-96 rounded-lg p-3 space-y-4 flex flex-col justify-center items-center">
@@ -21,49 +18,3 @@
     </main>
   </div>
 </template>
-
-<style scoped>
-.svg-bg {
-  position: absolute;
-  z-index: -1;
-  filter: blur(70px);
-  transform: translate3d(0, 0, 0); /* Fix for Safari flickering */
-  background-repeat: no-repeat;
-  width: 70%;
-  height: 60%;
-  will-change: filter;
-}
-
-.svg-bg-1 {
-  top: 20%;
-  left: 0%;
-  background-image: url("/Vector-1.svg");
-  /* animation: one 10s infinite; */
-}
-
-.svg-bg-2 {
-  top: 13%;
-  left: 25%;
-  background-image: url("/Vector-2.svg");
-  /* animation: two 10s infinite delay(0.5s); */
-}
-
-.svg-bg-3 {
-  top: 10%;
-  left: 30%;
-  background-image: url("/Vector-3.svg");
-  /* animation: one 10s infinite; */
-}
-
-.svg-bg-4 {
-  top: 10%;
-  left: 30%;
-  background-image: url("/Vector-4.svg");
-  /* animation: two 10s infinite delay(0.5s); */
-}
-
-/* .router-link-exact-active {
-  background-color: #90c0f7;
-  color: #fff;
-} */
-</style>
