ef14002b428b9647208c0470b276dadf6b91a0d4
VivienG-Dev <77700655+VivienG-Dev@users.noreply.github.com>
2024-07-01 21:46:43 +0200

    refactor: added validation middleware to specific routes

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -3,39 +3,25 @@ const router = require("express").Router();
 const userController = require("../controllers/user");
 
 const validateUsernameMiddleware = require("../middlewares/validateUsername");
+const validations = require("../middlewares/validation");
+const handleValidationErrors = require("../helpers/errors/validationErrorHandler");
 
-router.get("/", userController.getAllUsers); // ROUTE TO DELETE
+router.get("/:username", validateUsernameMiddleware, userController.getUserById);
 
-router.get(
-  "/:username",
-  validateUsernameMiddleware,
-  userController.getUserById
-);
-
-router.get(
-  "/:username/favorites",
-  validateUsernameMiddleware,
-  userController.getFavoriteCards
-);
+router.get("/:username/favorites", validateUsernameMiddleware, userController.getFavoriteCards);
 
 router.patch(
   "/:username",
   validateUsernameMiddleware,
+  validations.user.updateProfile,
+  handleValidationErrors,
   userController.updateUserData
 );
 
-router.delete(
-  "/:username/trash",
-  validateUsernameMiddleware,
-  userController.addUserInBin
-);
+router.delete("/:username/trash", validateUsernameMiddleware, userController.addUserInBin);
 
 router.post("/:username/untrash", userController.restoreUserFromBin);
 
-router.delete(
-  "/:username/delete",
-  validateUsernameMiddleware,
-  userController.deleteUserPermanently
-);
+router.delete("/:username/delete", validateUsernameMiddleware, userController.deleteUserPermanently);
 
 module.exports = router;
