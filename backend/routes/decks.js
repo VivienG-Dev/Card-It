bc15018ba5138e07c92adaeed6ec41d05f7e8df6
VivienG-Dev <77700655+VivienG-Dev@users.noreply.github.com>
2024-07-01 21:46:36 +0200

    refactor: added validation middleware to specific routes

--- backend/routes/decks.js
+++ backend/routes/decks.js
@@ -1,21 +1,33 @@
 // Using mergeParams to access :username from parent router (the route is /users/:username/decks)
 const router = require("express").Router({ mergeParams: true });
-
 const deckController = require("../controllers/deck");
-
 const validateUsernameMiddleware = require("../middlewares/validateUsername");
+const validations = require("../middlewares/validation");
+const handleValidationErrors = require("../helpers/errors/validationErrorHandler");
 
 router.get("/", validateUsernameMiddleware, deckController.getAllDecks);
 
 router.get("/:deckId", validateUsernameMiddleware, deckController.getOneDeck);
 
-router.patch("/:deckId", validateUsernameMiddleware, deckController.updateDeck);
+router.patch(
+  "/:deckId",
+  validateUsernameMiddleware,
+  validations.deck.update,
+  handleValidationErrors,
+  deckController.updateDeck
+);
 
 router.patch("/:deckId/generate-share-link", validateUsernameMiddleware, deckController.generateShareLink);
 
-router.put("/", validateUsernameMiddleware, deckController.createDeck);
+router.put("/", validateUsernameMiddleware, validations.deck.create, handleValidationErrors, deckController.createDeck);
 
-router.put("/accept-share/:shareToken", validateUsernameMiddleware, deckController.acceptSharedDeck);
+router.put(
+  "/accept-share/",
+  validateUsernameMiddleware,
+  validations.deck.acceptSharedDeck,
+  handleValidationErrors,
+  deckController.acceptSharedDeck
+);
 
 router.delete("/:deckId", validateUsernameMiddleware, deckController.deleteDeck);
 
